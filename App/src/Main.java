d3fe56edfbff2bbc8b3a276aa19cca76e38c7460
phamquan1a <160841249+phamquan1a@users.noreply.github.com>
2024-05-08 23:56:40 +0700

    Delete App directory

--- App/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        DictionaryCommandLine commandLine = new DictionaryCommandLine();
-        commandLine.performAction(8);
-        commandLine.runApplication();
-    }
-}
