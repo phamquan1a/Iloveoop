798f3f01cbeee73caed56fbc33c4f03fca90a99c
phamquan1a <160841249+phamquan1a@users.noreply.github.com>
2024-05-08 22:44:43 +0700

    Add files via upload

---
+++ App/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        DictionaryCommandLine commandLine = new DictionaryCommandLine();
+        commandLine.performAction(8);
+        commandLine.runApplication();
+    }
+}
